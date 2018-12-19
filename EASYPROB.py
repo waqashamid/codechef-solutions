88f1ec66a2abd06ce452d11797ac3ef201248c39
waqas <waqashamid722@gmail.com>
2018-12-19 19:51:14 +0530

    ACed

---
+++ EASYPROB.py
@@ -0,0 +1,17 @@
+# Authored by: Waqas Hamid
+# Github: waqashamid
+# Kaggle: waqashamid
+# Contact: waqas7hamid@gmail.com
+# Problem: https://www.codechef.com/problems/EASYPROB
+
+def main():
+    print("137=2(2(2)+2+2(0))+2(2+2(0))+2(0)")
+    print("1315=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2(0))+2+2(0)")
+    print("73=2(2(2)+2)+2(2+2(0))+2(0)")
+    print("136=2(2(2)+2+2(0))+2(2+2(0))")
+    print("255=2(2(2)+2+2(0))+2(2(2)+2)+2(2(2)+2(0))+2(2(2))+2(2+2(0))+2(2)+2+2(0)")
+    print("1384=2(2(2+2(0))+2)+2(2(2+2(0)))+2(2(2)+2)+2(2(2)+2(0))+2(2+2(0))")
+    print("16385=2(2(2+2(0))+2(2)+2)+2(0)")
+
+if __name__=='__main__':
+    main()
